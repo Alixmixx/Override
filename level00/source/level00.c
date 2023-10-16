f75f4f9c589652f4fe791f028fe5c8f594a2cd36
amuller <amuller@student.42.fr>
2023-10-16 15:17:16 -0400

    miao

---
+++ level00/source/level00.c
@@ -0,0 +1,23 @@
+#include 
+
+bool main(void)
+{
+	int array[4];
+
+	puts("***********************************");
+	puts("* \t     -Level00 -\t\t  *");
+	puts("***********************************");
+	printf("Password:");
+
+	scanf("%d", array);
+	if (array[0] != 5276)
+	{
+		puts("\nInvalid Password!");
+	}
+	else
+	{
+		puts("\nAuthenticated!");
+		system("/bin/sh");
+	}
+	return array[0] != 5276;
+}
