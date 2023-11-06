82b3cbf512e0c37d541c10adafae1ff768324b7f
Lorenzo <lorenzo.simanic@gmail.com>
2023-11-06 19:23:17 +0100

    level02 ok!

--- level02/source/level02.c
+++ level02/source/level02.c
@@ -1,40 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void)
 {
-    int password[14];
-    int pass[6];
-    int username[12];
-    int ret;
-    FILE *fd;
+	int password[41];
+	int pass[41];
+	int username[100];
+	int bytesRead;
+	FILE *passwordFile;
 
-    ret = 0;
-    fd = fopen("/home/users/level03/.pass", "r");
-    if (fd == 0)
-    {
-        fwrite("ERROR: failed to open password file\n", 1, 0x24, stderr);
-        exit(1);
-    }
-    ret = fread(pass, 1, 41, fd);
-    if (ret != 41)
-    {
-        fwrite("ERROR: failed to read password file\n", 1, 0x24, stderr);
-        exit(1);
-    }
-    fclose(fd);
-    puts("| You must login to access this system. |");
-    printf("--[ Username: ");
-    fgets(username, 100, stdin);
-    printf("--[ Password: ");
-    fgets(password, 100, stdin);
-    puts("*****************************************");
-    if (strncmp(pass, password, 41) == 0)
-    {
-        printf("Greetings, %s!\n", username);
-        system("/bin/sh");
-        return 0;
-    }
-    printf(username);
-    puts(" does not have access!");
-    exit(1);
+	memset(password, 0, 41);
+	memset(pass, 0, 41);
+	memset(username, 0, 100);
+
+	passwordFile = fopen("/home/users/level03/.pass", "r");
+	if (passwordFile == 0)
+	{
+		fwrite("ERROR: failed to open password file\n", 1, 36, stderr);
+		exit(1);
+	}
+	bytesRead = fread(pass, 1, 41, passwordFile);
+	if (bytesRead != 41)
+	{
+		fwrite("ERROR: failed to read password file\n", 1, 36, stderr);
+		exit(1);
+	}
+	fclose(passwordFile);
+
+	puts("| You must login to access this system. |");
+	printf("--[ Username: ");
+	fgets(username, 100, stdin);
+
+	printf("--[ Password: ");
+	fgets(password, 100, stdin);
+	puts("*****************************************");
+
+	if (strncmp(pass, password, 41) == 0)
+	{
+		printf("Greetings, %s!\n", username);
+		system("/bin/sh");
+		return 0;
+	}
+
+	printf(username);
+	puts(" does not have access!");
+	exit(1);
 }
