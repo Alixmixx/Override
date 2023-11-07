798ed70f9ae12cc7854a0c4ef94377c2756001ab
Lorenzo <lorenzo.simanic@gmail.com>
2023-11-07 06:54:29 +0100

    level05 ok

--- level04/source/level04.c
+++ level04/source/level04.c
@@ -8,40 +8,41 @@
 #include <sys/prctl.h>
 #include <wait.h>
 
-int main()
+int main(void)
 {
-    pid_t child = fork();
-    char buffer[128] = {0};
-    int syscall = 0;
-    int status = 0;
+	pid_t child = fork();
+	char buffer[128] = {0};
+	int syscall = 0;
+	int status = 0;
 
-    if(child == 0)
-    {
-        prctl(PR_SET_PDEATHSIG, SIGHUP);
-        ptrace(PTRACE_TRACEME, 0, NULL, NULL);
-        puts("just give me some shellcode, k");
-        gets(buffer);
-    }
-    else
-    {
-        while(1)
-        {
-            wait(&status);
-            if (WIFEXITED(status) || WIFSIGNALED(status)){
-                puts("child is exiting...");
-                break;
-            }
+	if (child == 0)
+	{
+		prctl(PR_SET_PDEATHSIG, SIGHUP);
+		ptrace(PTRACE_TRACEME, 0, NULL, NULL);
+		puts("just give me some shellcode, k");
+		gets(buffer);
+	}
+	else
+	{
+		while (1)
+		{
+			wait(&status);
+			if (WIFEXITED(status) || WIFSIGNALED(status))
+			{
+				puts("child is exiting...");
+				break;
+			}
 
-            syscall = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
+			syscall = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
 
-            if(syscall == 11)
-            {
-                printf("no exec() for you\n");
-                kill(child, SIGKILL);
-                break;
-            }
-        }
-    }
+			if (syscall == 11)
+			{
+				printf("no exec() for you\n");
+				kill(child, SIGKILL);
+				break;
+			}
+		}
+	}
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- level05/source/level05.c
+++ level05/source/level05.c
@@ -1,38 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 int main(void)
-
 {
-  char bVar1;
-  uint uVar2;
-  char *pbVar3;
-  char bVar4;
-  char buffer [100];
-  uint counter;
-  
-  bVar4 = 0;
-  counter = 0;
-  fgets(buffer,100,stdin);
-  counter = 0;
-  do {
-    uVar2 = 0xffffffff;
-    pbVar3 = buffer;
-    do {
-      if (uVar2 == 0) break;
-      uVar2 = uVar2 - 1;
-      bVar1 = *pbVar3;
-      pbVar3 = pbVar3 + (uint)bVar4 * -2 + 1;
-    } while (bVar1 != 0);
-    if (~uVar2 - 1 <= counter) {
-      printf(buffer);
-      exit(0);
-    }
-    if (('@' < buffer[counter]) && (buffer[counter] < '[')) {
-      buffer[counter] = buffer[counter] ^ 0x20;
-    }
-    counter = counter + 1;
-  } while( 1 );
+	int i = 0;
+	char buffer[100];
+
+	fgets(buffer, 100, stdin);
+
+	int len = strlen(buffer);
+	for (i = 0; i < len; i++)
+		if (buffer[i] >= 'A' && buffer[i] <= 'Z')
+			buffer[i] = buffer[i] ^ 0x20;
+
+	printf(buffer);
+	exit(EXIT_SUCCESS);
 }
